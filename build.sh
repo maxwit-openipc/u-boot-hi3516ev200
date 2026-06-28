0645bc8a08cfad58d4292dec07a6a7c96d94fdef
Conke Hu <conke.hu@maxwit.com>
2026-06-28 19:35:09 +0800

    init
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 export ARCH=arm
 export CROSS_COMPILE=${CROSS_COMPILE:-arm-himix100-linux-}
 
-OUTPUTDIR="${HOME}/src/hisi/uboot"
-SOCS="hi3516ev200 hi3516ev300 hi3518ev300 hi3516dv200"
+SOCS="hi3516ev200 hi3516ev300 hi3518ev300"
 
 for soc in ${SOCS};do
 
@@ -13,11 +12,10 @@ cp config-${soc} .config
 cp reg_info_${soc}.bin .reg
 make -j8 #KCFLAGS=-DCONFIG_XM_COMPATIBLE=1
 
-[ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip || cp tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ -rf
+[ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash || cp tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ -rf
 
 make u-boot-z.bin
 
-cp u-boot-${soc}.bin ${OUTPUTDIR}/u-boot-${soc}-universal.bin
-cp u-boot-${soc}.bin /srv/tftp/u-boot-${soc}-universal.bin
+cp -v u-boot-${soc}.bin /srv/tftp/
 
 done
