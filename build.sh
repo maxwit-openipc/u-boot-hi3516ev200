838be737a2ffdc7ea38502db1430b609fd58e375
Conke Hu <conke.hu@maxwit.com>
2026-06-29 16:10:27 +0800

    dtb fixed
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -4,12 +4,14 @@ test -z "$TOOLCHAIN" && TOOLCHAIN=arm-himix100-linux-
 
 for soc in hi3516ev200 hi3516ev300 hi3518ev300
 do
+    echo "Building u-boot for $soc"
+
     make clean
 
     make ${soc}_defconfig
 
     cp -v reg_info_${soc}.bin .reg
-    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${soc}-demb || exit 1
+    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${soc}-demb -j$(nproc) || exit 1
 
     [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash
     cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ || exit 1
