625f62abc07b45ce4d7a3ffeb4c9adab92237669
Conke Hu <conke.hu@maxwit.com>
2026-06-29 12:00:32 +0800

    remove redefined macros
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- include/configs/hi3556av100.h
+++ include/configs/hi3556av100.h
@@ -50,7 +50,6 @@
 #define CONFIG_SYS_TIMER_COUNTS_DOWN
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN       (CONFIG_ENV_SIZE + SZ_128K)
 
 /* PL011 Serial Configuration */
 #define CONFIG_BAUDRATE         115200
@@ -83,14 +82,12 @@
 #endif
 
 #ifdef CONFIG_HIFMC_SPI_NOR
-#define CONFIG_CMD_SF
 #define CONFIG_ENV_IS_IN_SPI_FLASH
 #define CONFIG_SPI_NOR_MAX_CHIP_NUM 1
 #define CONFIG_SPI_NOR_QUIET_TEST
 #endif
 
 #ifdef CONFIG_HIFMC_SPI_NAND
-#define CONFIG_CMD_NAND
 #define CONFIG_ENV_IS_IN_NAND
 #define CONFIG_SPI_NAND_MAX_CHIP_NUM    1
 #define CONFIG_SYS_MAX_NAND_DEVICE  CONFIG_SPI_NAND_MAX_CHIP_NUM
@@ -100,7 +97,6 @@
 
 #ifdef CONFIG_HIFMC_NAND
 /* #define CONFIG_NAND_EDO_MODE */
-#define CONFIG_CMD_NAND
 #define CONFIG_ENV_IS_IN_NAND
 #define CONFIG_NAND_MAX_CHIP_NUM    1
 #define CONFIG_SYS_MAX_NAND_DEVICE  CONFIG_NAND_MAX_CHIP_NUM
@@ -112,7 +108,6 @@
 #ifdef CONFIG_MMC
 #define CONFIG_SUPPORT_EMMC_BOOT
 #define CONFIG_GENERIC_MMC
-#define CONFIG_CMD_MMC
 #define CONFIG_ENV_IS_IN_MMC
 #define CONFIG_SYS_MMC_ENV_DEV  0
 #define CONFIG_EXT4_SPARSE
@@ -146,7 +141,6 @@
  */
 
 /* Assume we boot with root on the seventh partition of eMMC */
-#define CONFIG_BOOTARGS "mem=128M console=ttyAMA0,115200n8"
 #define CONFIG_BOOTCOMMAND "bootm 0x22000000 0x23000000"
 
 #define CONFIG_SYS_USB_XHCI_MAX_ROOT_PORTS 2
@@ -163,9 +157,7 @@
 #define CONFIG_COMMAND_HISTORY
 
 /* env in flash instead of CFG_ENV_IS_NOWHERE */
-#define CONFIG_ENV_OFFSET       0x80000      /* environment starts here */
 
-#define CONFIG_ENV_SIZE         0x40000
 #define CONFIG_ENV_SECT_SIZE        0x10000
 #define CONFIG_ENV_VARS_UBOOT_CONFIG
 
@@ -181,7 +173,6 @@
 #define CONFIG_CMDLINE_EDITING
 #define CONFIG_SYS_MAXARGS      64  /* max command args */
 
-#define CONFIG_SYS_NO_FLASH
 
 
 /*#define DDR_SCRAMB_ENABLE*/
@@ -214,7 +205,6 @@
 
 #ifndef CONFIG_GENERIC_MMC
 #define CONFIG_GENERIC_MMC
-#define CONFIG_CMD_MMC
 #endif
 
 #ifndef CONFIG_MMC
