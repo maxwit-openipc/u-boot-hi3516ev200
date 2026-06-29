5c74c3292b2f4ac84b48e634e616922c6b77cda2
Conke Hu <conke.hu@maxwit.com>
2026-06-29 13:56:44 +0800

    enable device tree
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ do
     make ${soc}_defconfig
 
     cp -v reg_info_${soc}.bin .reg
-    make CROSS_COMPILE=$TOOLCHAIN -j$(nproc) || exit 1
+    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${soc}-demb || exit 1
 
     [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash
     cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ || exit 1
