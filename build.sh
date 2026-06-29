ebbba6927781b4794555bb7e858fff54405975cf
Conke Hu <conke.hu@maxwit.com>
2026-06-29 09:21:53 +0800

    add var $TOOLCHAIN
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 mkdir -vp output
 
+TOOLCHAIN=arm-himix100-linux-
+# TOOLCHAIN=$PWD/../output-hi3516ev200/host/bin/arm-openipc-linux-musleabi-
+
 for soc in hi3516ev200 hi3516ev300 hi3518ev300
 do
     make clean
@@ -7,11 +10,11 @@ do
     cp -v config-${soc} .config
 
     cp -v reg_info_${soc}.bin .reg
-    make CROSS_COMPILE=arm-himix100-linux- -j$(nproc)
+    make CROSS_COMPILE=$TOOLCHAIN -j$(nproc) || exit 1
 
-    [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash || cp tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ -rf
+    [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash || cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/
 
-    make CROSS_COMPILE=arm-himix100-linux- u-boot-z.bin
+    make CROSS_COMPILE=$TOOLCHAIN u-boot-z.bin || exit 1
 
     cp -v u-boot-${soc}.bin output/
 
