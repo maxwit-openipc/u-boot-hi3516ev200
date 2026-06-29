1346a8e92ef4d8509bdf83b6107001378b5732c2
Conke Hu <conke.hu@maxwit.com>
2026-06-29 16:43:23 +0800

    init board support
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -2,16 +2,18 @@ mkdir -vp output
 
 test -z "$TOOLCHAIN" && TOOLCHAIN=arm-himix100-linux-
 
-for soc in hi3516ev200 hi3516ev300 hi3518ev300
+for board in hi3516ev200-demb hi3516ev300-demb hi3518ev300-demb
 do
-    echo "Building u-boot for $soc"
+    soc=${board%-demb} # FIXME
+
+    echo "Building u-boot for $soc ..."
 
     make clean
 
     make ${soc}_defconfig
 
     cp -v reg_info_${soc}.bin .reg
-    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${soc}-demb -j$(nproc) || exit 1
+    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${board} -j$(nproc) || exit 1
 
     [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash
     cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ || exit 1
