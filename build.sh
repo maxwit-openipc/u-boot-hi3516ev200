7677919069f39d89171ed717489a6f6a99134a8a
Conke Hu <conke.hu@maxwit.com>
2026-06-30 11:57:46 +0800

    misc upgrade
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,35 @@
-mkdir -vp output
+#!/usr/bin/env bash
 
-test -z "$TOOLCHAIN" && TOOLCHAIN=arm-himix100-linux-
+if [ $# -gt 0 ]; then
+	board_list=$1
+else
+	board_list="
+	hi3516ev200-demb
+	hi3516ev300-demb
+	hi3518ev300-demb
+	"
+fi
 
-for board in hi3516ev200-demb hi3516ev300-demb hi3518ev300-demb
+test -z $OUTPUT && OUTPUT=output
+# rm -vf $OUTPUT/u-boot*
+mkdir -p $OUTPUT
+
+if [ -z "$TOOLCHAIN" ]; then
+	for t in arm-himix100-linux- arm-none-eabi-
+	do
+		if which ${t}gcc > /dev/null; then
+			TOOLCHAIN=$t
+			break
+		fi
+	done
+fi
+
+if [ -z $TOOLCHAIN ]; then
+	echo "No toolchain found!"
+	exit 1
+fi
+
+for board in $board_list
 do
     soc=${board%-demb} # FIXME
 
@@ -13,14 +40,14 @@ do
     make ${soc}_defconfig
 
     cp -v reg_info_${soc}.bin .reg
-    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${board} -j$(nproc) || exit 1
+    make CROSS_COMPILE=$TOOLCHAIN DEVICE_TREE=${board} $XOPT || exit 1
 
     [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash
     cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ || exit 1
 
     make CROSS_COMPILE=$TOOLCHAIN u-boot-z.bin || exit 1
 
-    cp -v u-boot-${soc}.bin u-boot-${soc}-universal.bin
+    # cp -v u-boot-${soc}.bin u-boot-${soc}-universal.bin
     cp -v u-boot-${soc}.bin output/u-boot-${board}.bin
 
     echo
