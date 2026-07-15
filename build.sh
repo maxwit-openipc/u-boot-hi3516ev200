42cf2cd1a28d7659cf2e51cb27c38613345965f9
Conke Hu <conke.hu@maxwit.com>
2026-07-15 11:02:41 +0800

    add ci-build.sh and keep build.sh same with upstream
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build.sh
+++ build.sh
@@ -1,81 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 
-OUTPUT=${OUTPUT:-$PWD/output}
-XOPT=${XOPT:-V=1}
-TARGET_BOARD=$1
+export ARCH=arm
+export CROSS_COMPILE=${CROSS_COMPILE:-arm-himix100-linux-}
 
-count=0
-for dts in `ls arch/arm/dts/*.dts`
-do
-    chip_ids=($(grep -m1 -o '"hisilicon,hi35[0-9]\+[adce]v[1-9]00";' $dts | sed 's/[",;]/ /g'))
-    test ${#chip_ids[@]} -ne 2 && continue
+OUTPUTDIR="${HOME}/src/hisi/uboot"
+SOCS="hi3516ev200 hi3516ev300 hi3518ev300 hi3516dv200"
 
-    # vendor=${chip_ids[0]}
-    soc=${chip_ids[1]}
+for soc in ${SOCS};do
 
-    board=$(grep -m1 'compatible' $dts | awk -F ',' '{print $2}' | sed 's/[",;]//g')
-    test -n "$TARGET_BOARD" -a "$TARGET_BOARD" != $board && continue
+make clean
+cp config-${soc} .config
+cp reg_info_${soc}.bin .reg
+make -j8 #KCFLAGS=-DCONFIG_XM_COMPATIBLE=1
 
-    for tc in arm-openipc-linux-musleabi- \
-        arm-linux-musleabi- \
-        arm-linux-gnueabi- \
-        arm-linux- \
-        arm-none-eabi-
-    do
-        for out in $PWD/output $(dirname $PWD)/output $OUTPUT
-        do
-            path=$out/$soc/host/bin
-            if test -e $path/${tc}gcc; then
-                toolchain=$path/$tc
-                break
-            fi
-        done
+[ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip || cp tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/${soc}/hw_compressed/ -rf
 
-        test -n "$toolchain" && break
+make u-boot-z.bin
 
-        if which ${tc}gcc > /dev/null; then
-            toolchain=$tc
-            break
-        fi
-    done
+cp u-boot-${soc}.bin ${OUTPUTDIR}/u-boot-${soc}-universal.bin
+cp u-boot-${soc}.bin /srv/tftp/u-boot-${soc}-universal.bin
 
-    if [ -z "$toolchain" ]; then
-        echo "No toolchain found for $soc!"
-        echo "Skip to build u-boot for $board!"
-        echo
-        continue
-    fi
-
-    echo "Building u-boot for $board ($soc) ..."
-
-    make distclean
-
-    make ${soc}_defconfig
-
-    dtb=$(basename ${dts%.dts})
-    cp -v reg_info_${soc}.bin .reg
-    make CROSS_COMPILE=$toolchain DEVICE_TREE=$dtb $XOPT || exit 1
-
-    [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash
-    cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/$soc/hw_compressed/ || exit 1
-
-    make CROSS_COMPILE=$toolchain u-boot-z.bin || exit 1
-
-    outfile="u-boot-${soc}.bin"
-    outsize=$(stat -c %s $outfile)
-    if [ "$outsize" -gt $((256 << 10)) ]; then
-        echo "$outfile size is to large ($((outsize >> 10))K)!"
-        exit 1
-    fi
-
-    # cp -v u-boot-${soc}.bin u-boot-${soc}-universal.bin
-    mkdir -vp $OUTPUT/$soc
-    cp -v $outfile $OUTPUT/$soc/u-boot-${board}.bin
-
-    ((count++))
-    echo
-    test -n "$TARGET_BOARD" -a "$TARGET_BOARD" == $board && break
 done
-
-echo "Total $count boards was built."
-echo
---
+++ ci-build.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+target_board=$1
+OUTPUT=${OUTPUT:-$PWD/output}
+XOPT=${XOPT:-V=1}
+
+count=0
+for dts in `ls arch/arm/dts/*.dts`
+do
+    chip_ids=($(grep -m1 -o '"hisilicon,hi35[0-9]\+[adce]v[1-9]00";' $dts | sed 's/[",;]/ /g'))
+    test ${#chip_ids[@]} -ne 2 && continue
+
+    # vendor=${chip_ids[0]}
+    soc=${chip_ids[1]}
+
+    board=$(grep -m1 'compatible' $dts | awk -F ',' '{print $2}' | sed 's/[",;]//g')
+    test -n "$target_board" -a "$target_board" != $board && continue
+
+    for tc in arm-openipc-linux-musleabi- \
+        arm-linux-musleabi- \
+        arm-linux-gnueabi- \
+        arm-linux- \
+        arm-none-eabi-
+    do
+        for out in $PWD/output $(dirname $PWD)/output $OUTPUT
+        do
+            path=$out/$soc/host/bin
+            if test -e $path/${tc}gcc; then
+                toolchain=$path/$tc
+                break
+            fi
+        done
+
+        test -n "$toolchain" && break
+
+        if which ${tc}gcc > /dev/null; then
+            toolchain=$tc
+            break
+        fi
+    done
+
+    if [ -z "$toolchain" ]; then
+        echo "No toolchain found for $soc!"
+        echo "Skip to build u-boot for $board!"
+        echo
+        continue
+    fi
+
+    echo "Building u-boot for $board ($soc) ..."
+
+    make distclean
+
+    make ${soc}_defconfig
+
+    dtb=$(basename ${dts%.dts})
+    cp -v reg_info_${soc}.bin .reg
+    make CROSS_COMPILE=$toolchain DEVICE_TREE=$dtb $XOPT || exit 1
+
+    [ ! -f tools/hi_gzip/bin/gzip ] && make -C tools/hi_gzip SHELL=/bin/bash
+    cp -v tools/hi_gzip/bin/gzip arch/arm/cpu/armv7/$soc/hw_compressed/ || exit 1
+
+    make CROSS_COMPILE=$toolchain u-boot-z.bin || exit 1
+
+    outfile="u-boot-${soc}.bin"
+    outsize=$(stat -c %s $outfile)
+    if [ "$outsize" -gt $((256 << 10)) ]; then
+        echo "$outfile size is to large ($((outsize >> 10))K)!"
+        exit 1
+    fi
+
+    # cp -v u-boot-${soc}.bin u-boot-${soc}-universal.bin
+    mkdir -vp $OUTPUT/$soc
+    cp -v $outfile $OUTPUT/$soc/u-boot-${board}.bin
+
+    ((count++))
+    echo
+
+    test "$target_board" == $board && break
+done
+
+echo "Total $count boards was built."
+echo
